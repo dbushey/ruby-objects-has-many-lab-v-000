88b57b9ce5c17328100e6bfb736c03a75541da8c
Daiane Bushey <daianebushey@gmail.com>
2018-03-06 17:53:39 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,8 +10,8 @@ class Artist
 
   def add_song(song)
     @songs << song
-
     song.artist = self
+    @@song_count += 1
   end
 
   def songs
@@ -26,7 +26,6 @@ class Artist
     song = Song.new(name)
     @songs << song
     song.artist = self
-    @@song_count += 1
   end
 
 end
