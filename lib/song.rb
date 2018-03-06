a14e9c49cd10b2f363de796683757de387a225b6
Daiane Bushey <daianebushey@gmail.com>
2018-03-06 17:23:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,9 @@
-class Artist
-  attr_accessor :name, :genre
-
-  def initialize(name, genre)
-    @name = name
-    @genre = genre
+class Song
+ 
+  attr_accessor :title, :artist
+ 
+  def initialize(title)
+    @title = title
   end
-
+ 
 end
